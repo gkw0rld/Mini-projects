5b392ee4be2e37ff7c0940857cc86d5eb885eed3
Gaurav Khatri <115532424+gkw0rld@users.noreply.github.com>
2024-10-14 01:31:32 +0530

    Add files via upload

---
+++ Tic-Tac-Toe/src/components/Strike.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+function Strike({strikeClass})
+{
+    return (
+        <div className = {`strike ${strikeClass}`}>
+            
+        </div>
+    );
+}
+export default Strike;
